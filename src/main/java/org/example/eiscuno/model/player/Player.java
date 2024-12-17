8f79ad6cc8b017851dd08487b282d0fcbb664791
Libardo Alejandro Quintero Gómez <quintero.libardo@correounivalle.edu.co>
2024-12-17 12:13:33 -0500

    Saving prepull

--- src/main/java/org/example/eiscuno/model/player/Player.java
+++ src/main/java/org/example/eiscuno/model/player/Player.java
@@ -20,8 +20,8 @@ public class Player implements IPlayer, ShiftEventListener {
      */
     public Player(String typePlayer){
         this.cardsPlayer = new ArrayList<Card>();
-        Card card1 =  new Card("/org/example/eiscuno/cards-uno/2_wild_draw_green.png", "TwoWildDraw", "GREEN");
-        Card card2 =  new Card("/org/example/eiscuno/cards-uno/2_wild_draw_blue.png", "TwoWildDraw", "BLUE");
+        Card card1 =  new Card("/org/example/eiscuno/cards-uno/wild.png", "Wild", "MULTICOLOR");
+        Card card2 =  new Card("/org/example/eiscuno/cards-uno/4_wild_draw.png", "FourWildDraw", "MULTICOLOR");
         cardsPlayer.add(card1);
         cardsPlayer.add(card2);
         this.typePlayer = typePlayer;
